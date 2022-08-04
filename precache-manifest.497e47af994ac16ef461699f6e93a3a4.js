0d8b39791a3c26bf1c586faa574947ea784faead
hzlgrce <hgaflores@iskolarngbayan.pup.edu.ph>
2022-08-04 23:35:49 +0800

    Updates

---
+++ precache-manifest.497e47af994ac16ef461699f6e93a3a4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b23269c7516b556757ec",
+    "url": "/my-portfolio/static/css/main.7ff7902f.chunk.css"
+  },
+  {
+    "revision": "b23269c7516b556757ec",
+    "url": "/my-portfolio/static/js/main.b23269c7.chunk.js"
+  },
+  {
+    "revision": "d4d95a87c7a02a69aacb",
+    "url": "/my-portfolio/static/js/runtime~main.d4d95a87.js"
+  },
+  {
+    "revision": "5ec21d738fba382f2658",
+    "url": "/my-portfolio/static/js/2.5ec21d73.chunk.js"
+  },
+  {
+    "revision": "8ad0a27e72524604e49ea6de41fd3b18",
+    "url": "/my-portfolio/index.html"
+  }
+];
