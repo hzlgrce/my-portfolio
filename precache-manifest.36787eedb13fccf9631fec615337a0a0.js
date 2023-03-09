6aa40bc00a723922cd7c08665aba0a4cba6c5fe2
hgf <70837241+hzlgrce@users.noreply.github.com>
2023-03-09 17:04:19 +0000

    Revert "empty branch"
    This reverts commit fcfa0f31d0f82a1038b12213a7e096f313b8c9f7.

---
+++ precache-manifest.36787eedb13fccf9631fec615337a0a0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "68842c9d91e50cd985ea",
+    "url": "/my-portfolio/static/css/main.7ff7902f.chunk.css"
+  },
+  {
+    "revision": "68842c9d91e50cd985ea",
+    "url": "/my-portfolio/static/js/main.68842c9d.chunk.js"
+  },
+  {
+    "revision": "d4d95a87c7a02a69aacb",
+    "url": "/my-portfolio/static/js/runtime~main.d4d95a87.js"
+  },
+  {
+    "revision": "808b3dbcffceccd5f781",
+    "url": "/my-portfolio/static/js/2.808b3dbc.chunk.js"
+  },
+  {
+    "revision": "3f069e196f301166c445c895a7381de2",
+    "url": "/my-portfolio/index.html"
+  }
+];
